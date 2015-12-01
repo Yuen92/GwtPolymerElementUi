bcfa0797453dc44dcf81c5359909846c6ce9e63c
Yuen92 <bianconithomas@gmail.com>
2015-12-01 09:55:13 +0100

    Init

---
+++ GwtPolymerElementUi/src/gwt/polymer/elementui/client/GwtPolymerElementUi.java
@@ -0,0 +1,9 @@
+package gwt.polymer.elementui.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class GwtPolymerElementUi implements EntryPoint {
+    public void onModuleLoad() {
+
+    }
+}
