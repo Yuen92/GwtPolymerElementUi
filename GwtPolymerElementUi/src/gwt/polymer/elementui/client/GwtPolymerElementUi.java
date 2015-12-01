a415d1f47213e4b1be7d8d32523c1352ac2004a7
Yuen92 <bianconithomas@gmail.com>
2015-12-01 11:58:43 +0100

    Load iron-icons

--- GwtPolymerElementUi/src/gwt/polymer/elementui/client/GwtPolymerElementUi.java
+++ GwtPolymerElementUi/src/gwt/polymer/elementui/client/GwtPolymerElementUi.java
@@ -1,9 +1,32 @@
 package gwt.polymer.elementui.client;
 
+import gwt.polymer.elementui.client.composite.Main;
+
+import java.util.Arrays;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.elemental.Function;
+import com.vaadin.polymer.iron.element.IronIconElement;
+import com.vaadin.polymer.paper.element.PaperIconItemElement;
 
 public class GwtPolymerElementUi implements EntryPoint {
     public void onModuleLoad() {
+	// We have to load icon sets before run application
+	Polymer.importHref(Arrays.asList(
+		"iron-icons/iron-icons.html",
+		PaperIconItemElement.SRC,
+		IronIconElement.SRC), new Function() {
+	    public Object call(Object arg) {
+		// The app is executed when all imports succeed.
+		startApplication();
+		return null;
+	    }
+	});
+    }
 
+    private void startApplication() {
+	RootPanel.get().add(new Main());
     }
 }
